01c4bfada5f0c5053b0e72fc076e340f5b27459f
ThogoGakinya <thogosamson@gmail.com>
2023-12-29 12:55:34 +0300

    Returning a string without last Character - Utilization of Substring Method

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program {
+    public void RemoveLastCharacter(string value)
+    {
+        if(string.IsNullOrEmpty(value))
+        {
+            Console.WriteLine("Null");
+        }
+        else
+        {
+            string newString = value.Substring(0, value.Length -1);
+            Console.WriteLine(newString);
+                
+        }
+    }
+    public static void Main()
+    {
+        Program withoutLast = new Program();
+        withoutLast.RemoveLastCharacter("Samson");
+    }
+}
